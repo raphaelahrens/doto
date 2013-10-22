a9b00173765e70677ab0119e1f3a5a2d3fc34e8d
Tant <tant.sinnister@googlemail.com>
2013-10-22 18:08:47 +0200

    worked on tasks

---
+++ task.py
@@ -0,0 +1,84 @@
+import json
+import time
+import subprocess
+
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    keys = list(value for value in enums.iterkeys())
+    ident = enums.copy()
+    revert = dict((value, key) for key, value in enums.iteritems())
+    enums['keys'] = keys
+    enums['ident'] = ident.get
+    enums['revert'] = revert.get
+    return type('Enum', (), enums)
+
+STATE = enum("completed", "pending", "deleted")
+
+status_colors = {
+    STATE.completed: "darkolivegreen4",
+    STATE.pending:   "cornflowerblue",
+    STATE.deleted:   "red",
+}
+
+
+class Task:
+    def __init__(self, title, description, created, due, started):
+        self.title = unicode(title)
+        self.description = unicode(description)
+        self.created = created
+        self.due = due
+        self.started = started
+
+
+class TWTask(Task):
+    def __init__(self, task_dict):
+        def get_attr(string, default):
+            if str in task_dict:
+                return task_dict
+            else:
+                return default
+
+        def get_time(s, d):
+            return time.strptime(d[s], "%Y%m%dT%H%M%SZ") if s in d else None
+
+        _description = "description"
+        Task.__init__(self, task_dict[_description][0:100], task_dict[_description],
+                      get_time("entry", task_dict),
+                      get_time("due", task_dict),
+                      get_time("start", task_dict)
+                      )
+        if "depends" in task_dict:
+            self.depends = task_dict["depends"].split(",")
+        else:
+            self.depends = []
+
+        self._id = task_dict["id"]
+        self.uuid = task_dict["uuid"]
+        self.status = STATE.ident(task_dict["status"])
+        self.urgency = task_dict["urgency"]
+
+        self.end = get_attr("end", None)
+        self.recur = get_attr("recur", None)
+        self.tags = get_attr("tags", None)
+        self.modified = get_attr("modified", None)
+        self.project = get_attr("project", None)
+
+        self.data = task_dict
+
+    def __str__(self):
+        return unicode("uuid: %s\n Name: %s \n(%s)\n\n %s" % (self.uuid, self.description, self.status, self.data))
+
+    def __repr__(self):
+        return "(%s, %s)" % (self.uuid, self.description)
+
+
+class TaskwarrioirStore:
+    def __init__(self):
+        output = subprocess.check_output(["task", "export"])
+        items = map(TWTask, json.loads("[" + output + "]"))
+        self.index = dict((x.uuid, x) for x in items)
+        self.pending = [x for x in items if x.status == STATE.pending]
+
+    def get_tasks(self):
+        return self.pending
---
+++ task_view.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+# example helloworld.py
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pango
+import time
+
+import task
+
+
+class TaskInfo(gtk.Window):
+    """
+    This is a GUI to view the Task of the Done!Tools project.
+    """
+
+    def delete_event(self, widget, event, data=None):
+        """
+        Raction to the delete event.
+        """
+        return False
+
+    def destroy(self, widget, data=None):
+        """
+        This method ends the application
+        """
+        gtk.main_quit()
+
+    def __init__(self):
+        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+
+        self.connect("delete_event", self.delete_event)
+
+        self.connect("destroy", self.destroy)
+
+        self.summaryview = SummaryView()
+
+        self.notebook = gtk.Notebook()
+        self.notebook.set_tab_pos(gtk.POS_BOTTOM)
+        lblSummary = gtk.Label("Summary")
+#        lblComments = gtk.Label("Comments")
+#        lblFiles = gtk.Label("Files")
+        self.notebook.append_page(self.summaryview.getLayout(), lblSummary)
+
+        hbox1 = gtk.HBox()
+        hbox1.pack_start(self.notebook)
+        hbox1.show_all()
+        self.add(hbox1)
+        self.show()
+
+    def main(self, tasks):
+        self.summaryview.show_task(tasks[0])
+        gtk.main()
+
+
+class SummaryView:
+    """
+    the summary view of the task
+    """
+
+    def __init__(self):
+        """
+        set the view layout
+        """
+        self.vbox1 = gtk.VBox()
+        self.lblTaskTitle = gtk.Label()
+        self.lblTaskTitle.set_alignment(0.0, 0.5)
+        self.lblTaskTitle.set_padding(10, 5)
+        font = pango.FontDescription()
+        font.set_size(14000)
+        font.set_weight(pango.WEIGHT_BOLD)
+        self.lblTaskTitle.modify_font(font)
+        self.vbox1.pack_start(self.lblTaskTitle, expand=False)
+
+        font = pango.FontDescription()
+        font.set_weight(pango.WEIGHT_BOLD)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+        tvDescription = gtk.TextView()
+        tvDescription.set_size_request(600, -1)
+        #tvDescription.set_justify(gtk.JUSTIFY_LEFT)
+        #tvDescription.set_alignment(0.0, 0.0)
+        #tvDescription.set_padding(20, 0)
+        tvDescription.set_left_margin(20)
+        tvDescription.set_wrap_mode(gtk.WRAP_WORD)
+        tvDescription.set_editable(False)
+        tvDescription.set_accepts_tab(True)
+        tvDescription.set_cursor_visible(False)
+
+        self.tbDescription = tvDescription.get_buffer()
+
+        sw.add(tvDescription)
+        self.vbox1.pack_start(sw)
+
+        self.lblOriginator = gtk.Label()
+        self.lblOriginator.modify_font(font)
+        self.lblOriginator.set_padding(10, 0)
+
+        self.imgOriginator = gtk.Image()
+        self.imgOriginator.set_size_request(130, 130)
+
+        self.lblOwner = gtk.Label()
+        self.lblOwner.modify_font(font)
+        self.lblOwner.set_padding(10, 0)
+
+        self.imgOwner = gtk.Image()
+        self.imgOwner.set_size_request(130, 130)
+
+        self.lblCreated = gtk.Label()
+        self.lblCreated.modify_font(font)
+        self.lblCreated.set_padding(10, 0)
+
+        self.lblStarted = gtk.Label()
+        self.lblStarted.modify_font(font)
+        self.lblStarted.set_padding(10, 0)
+
+        self.lblDue = gtk.Label()
+        self.lblDue.modify_font(font)
+        self.lblDue.set_padding(10, 0)
+
+        vboxOriginator = gtk.VBox()
+        vboxOriginator.pack_start(self.lblOriginator)
+        vboxOriginator.pack_start(self.imgOriginator)
+
+        vboxOwner = gtk.VBox()
+        vboxOwner.pack_start(self.lblOwner)
+        vboxOwner.pack_start(self.imgOwner)
+
+        hboxUser = gtk.HBox()
+        hboxUser.pack_start(vboxOriginator)
+        hboxUser.pack_start(vboxOwner)
+
+        self.vbox1.pack_start(hboxUser, expand=False)
+
+        hboxDates = gtk.HBox()
+        hboxDates.pack_start(self.lblCreated, fill=False)
+        hboxDates.pack_start(self.lblStarted)
+        hboxDates.pack_start(self.lblDue)
+        self.vbox1.pack_start(hboxDates, expand=False, fill=False)
+
+        self.vbox1.show_all()
+
+    def show_task(self, task):
+        self.lblTaskTitle.set_text(task.title)
+
+        self.lblOriginator.set_text("Rapahel")
+        self.lblOwner.set_text("Sarah")
+
+        self.imgOriginator.set_from_file("./avatars/Bwoob.jpg")
+
+        self.tbDescription.set_text(task.description)
+        self.lblCreated.set_text(time.asctime(task.created))
+        if task.due:
+            self.lblDue.set_text(time.asctime(task.due))
+        else:
+            self.lblDue.set_text(time.asctime(time.gmtime()))
+        if task.started:
+            self.lblStarted.set_text(time.asctime(task.started))
+        else:
+            self.lblStarted.set_text(time.asctime(time.gmtime()))
+#       self.lblOriginator.set_text(task.originator)
+#       self.lblOwner.set_text(task.owner)
+
+    def getLayout(self):
+        return self.vbox1
+
+if __name__ == "__main__":
+    tw_store = task.TaskwarrioirStore()
+    hello = TaskInfo()
+    hello.main(tw_store.get_tasks())
