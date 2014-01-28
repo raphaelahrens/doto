dc7d68be8bc3c6f331ea35fa3c4301ff620d0e4f
Tant <tant.sinnister@googlemail.com>
2014-01-28 17:48:02 +0100

    Fixed from input format string

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ TOOLS_PATH = "/home/tant/pomodoro/"
 
 USER_PATH = os.path.expanduser("~") + "/.doto/"
 
-DATE_OUT_STR = "%d. %b. %Y"  # %H:%m"
-DATE_INPUT_STR = "%Y%m%d-%H:%M"  # %H:%m"
+TASK_STORE = USER_PATH + "store.db"
+
+DATE_OUT_STR = "%d. %b. %Y"
+DATE_INPUT_STR = "%Y.%m.%d-%H:%M"
 
 LOCAL_TZ = "Europe/Berlin"
