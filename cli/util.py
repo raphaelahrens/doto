2e6593c207ea0facef10174a3d39c7e82f7737e0
Tant <tant.sinnister@googlemail.com>
2014-02-18 22:26:41 +0100

    changed the print table function
    replaced the selfwritten code with the str.format function

--- cli/util.py
+++ cli/util.py
@@ -1,6 +1,5 @@
-__all__ = ["uprint", "print_table"]
-
-import itertools
+# -*- coding: utf-8 -*-
+__all__ = ["uprint", "print_table", ]
 
 
 def uprint(string):
@@ -13,50 +12,37 @@ def to_str(obj):
     return str(obj)
 
 
-def align(string, length, orientation="left"):
-    extra_space = length - len(string)
-    if extra_space < 0:
-        extra_space = 0
-    if orientation == "middle":
-        side_space = " " * ((extra_space) / 2)
-        return side_space + string + side_space + " " * (extra_space % 2)
-    if orientation == "left":
-        return string + extra_space * " "
-    if orientation == "right":
-        return " " * extra_space + string
-
-
 lines = {"v": unichr(0x2502),
          "+": unichr(0x253c),
          "h": unichr(0x2500)
          }
 
 
-def print_column_names(column_names):
-    if len(column_names) < 1:
-        raise Exception("too few column_names")
-    lengths = []
-    header_strings = []
-    dividers = []
-    for header, length in column_names:
-        if length < len(header):
-            length = len(header)
-        lengths.append(length)
-        header_strings.append(align(header, length, "middle"))
-        dividers.append(lines["h"] * length)
-    uprint(lines["v"].join(header_strings))
-    uprint(lines["+"].join(dividers))
-    return lengths
+def print_table(columns, data):
+    table = Table(columns)
+    table.print_multiple_rows(data)
 
 
-def print_line(values, lengths):
-    if len(values) != len(lengths):
-        raise Exception("too few values")
-    strings = [align(to_str(value)[:length], length, "left") for value, length in itertools.izip(values, lengths)]
-    return lines["v"].join(strings)
+class Table(object):
+    def __init__(self, columns):
+        self._column_count = len(columns)
+        header = []
+        divider = []
+        row_format = []
+        for name, width in columns:
+            if width < len(name):
+                width = len(name)
+            header.append(("{:%s%d}" % ("^", width)).format(name))
+            divider.append(lines["h"] * width)
+            row_format.append("{:%s%d}" % ("<", width))
+        uprint(lines["v"].join(header))
+        uprint(lines["+"].join(divider))
+        self._row_format = lines["v"].join(row_format)
 
+    def print_row(self, data):
+        assert(len(data) == self._column_count)
+        uprint(self._row_format.format(*data))
 
-def print_table(column_names, data):
-    lengths = print_column_names(column_names)
-    for values in data:
-        uprint(print_line(values, lengths))
+    def print_multiple_rows(self, data_list):
+        for data in data_list:
+            self.print_row(data)
