c636af5e84bae250edf4d8ee61c04d0b886a83b0
Raphael Ahrens <raphael.ahrens@fit.fraunhofer.de>
2017-04-30 16:59:46 +0200

    Added setup.py and added help for calling `doto`

--- doto/__init__.py
+++ doto/__init__.py
@@ -26,7 +26,7 @@ def init_env(commands):
     subparsers = parser.add_subparsers(help='command', dest="cmd")
     for cmd in commands:
         cmd.init_parser(subparsers)
-    return parser.parse_args()
+    return parser, parser.parse_args()
 
 
 def main():
@@ -42,10 +42,13 @@ def main():
     # Init phase
     cmds = doto.cli.sub_cmds.import_commands(doto.cli.cmd.__path__, doto.cli.cmd.__name__)
     config = doto.defaultconfig.read_config()
-    args = init_env(cmds.values())
+    parser, args = init_env(cmds.values())
     doto.cli.parser.set_date_parser(config.date.local_tz, config.date.cli_input_str)
     term = shutil.get_terminal_size()
     with doto.model.Store(config.path.store, config.path.cache) as store:
         # execute command
+        if args.cmd not in cmds:
+            parser.print_help()
+            return -1
         exit_code = cmds[args.cmd].main(store, args, config, term)
     return exit_code
